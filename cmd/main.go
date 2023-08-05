daf10a6eaebc22518abe29170df7a0e4a54ff49a
dofferni <dofferni@quoter.com>
2023-08-05 14:08:05 -0700

    initializing GQL boilerplate and tweaking servers

--- cmd/main.go
+++ cmd/main.go
@@ -1,38 +1,81 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"sync"
 
-	"github.com/joho/godotenv"
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/go-chi/chi"
+	"github.com/labstack/echo"
+	"github.com/offerni/hill-charts-api/graph"
+	"github.com/offerni/hill-charts-api/graph/generated"
+	"github.com/rs/cors"
 )
 
+const graphQLDefaultPort = "9092"
+const httpDefaultPort = "9091"
+
 func main() {
-	if err := godotenv.Load(); err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		initRESTServer()
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		initGraphQLServer()
+	}()
+
+	wg.Wait()
+}
+
+func initGraphQLServer() {
+	router := chi.NewRouter()
+
+	router.Use(cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowCredentials: true,
+		Debug:            true,
+	}).Handler)
+	port := os.Getenv("GRAPHQL_PORT")
+	if port == "" {
+		port = graphQLDefaultPort
+	}
+
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, router))
+}
+
+func initRESTServer() {
+	port := os.Getenv("HTTP_PORT")
+	if port == "" {
+		port = httpDefaultPort
 	}
 
 	e := echo.New()
-	e.Use(middleware.CORS())
+	setHttpRoutes(e)
 
-	helloHandler := func(c echo.Context) error {
-		return c.JSON(http.StatusOK, fmt.Sprintf("Hello World!"))
+	log.Printf("connect to http://localhost:%s/ for the REST API Server", port)
+	if err := e.Start(":" + port); err != http.ErrServerClosed {
+		log.Fatal(err)
 	}
 
-	e.GET("/hello", helloHandler)
+}
+
+func setHttpRoutes(e *echo.Echo) {
 	e.GET("/health", func(c echo.Context) error {
 		return c.NoContent(http.StatusNoContent)
 	})
-
-	port := os.Getenv("PORT")
-	defaultPort := "8080"
-	if port == "" {
-		port = defaultPort
-	}
-
-	e.Logger.Fatal((e.Start(fmt.Sprintf(":%s", port))))
 }
--- go.mod
+++ go.mod
@@ -3,20 +3,25 @@ module github.com/offerni/hill-charts-api
 go 1.20
 
 require (
-	github.com/joho/godotenv v1.5.1
-	github.com/labstack/echo/v4 v4.11.1
+	github.com/99designs/gqlgen v0.17.16
+	github.com/go-chi/chi v1.5.4
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/rs/cors v1.9.0
+	github.com/vektah/gqlparser/v2 v2.5.8
 )
 
 require (
-	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/agnivade/levenshtein v1.1.1 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.11.0 // indirect
 	golang.org/x/net v0.12.0 // indirect
 	golang.org/x/sys v0.10.0 // indirect
 	golang.org/x/text v0.11.0 // indirect
-	golang.org/x/time v0.3.0 // indirect
 )
